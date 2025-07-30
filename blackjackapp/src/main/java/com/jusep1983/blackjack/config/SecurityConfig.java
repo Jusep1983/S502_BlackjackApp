c561da8f54fdb076e4457f22a478b09688378bdb
Jusep1983 <j.lopezcalvo1983@gmail.com>
2025-07-30 15:59:49 +0200

    feat:config cors to render

--- blackjackapp/src/main/java/com/jusep1983/blackjack/config/SecurityConfig.java
+++ blackjackapp/src/main/java/com/jusep1983/blackjack/config/SecurityConfig.java
@@ -72,7 +72,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173",
+                "https://s502-blackjack-app-frontend.onrender.com"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
