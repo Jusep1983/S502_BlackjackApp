62ad24a1d763e1c372721f25b59c49cde5f1bac8
Jusep1983 <j.lopezcalvo1983@gmail.com>
2025-08-02 02:30:02 +0200

    progress after removing .env from staging

--- blackjackapp/src/main/java/com/jusep1983/blackjack/BlackjackApplication.java
+++ blackjackapp/src/main/java/com/jusep1983/blackjack/BlackjackApplication.java
@@ -1,17 +1,10 @@
 package com.jusep1983.blackjack;
 
-import com.jusep1983.blackjack.player.PlayerRepository;
-import com.jusep1983.blackjack.shared.enums.Role;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-//@EnableR2dbcRepositories(basePackages = "com.jusep1983.blackjack.player")
-//@EnableReactiveMongoRepositories(basePackages = "com.jusep1983.blackjack.game")
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class BlackjackApplication {
 
@@ -20,5 +13,3 @@ public class BlackjackApplication {
     }
 
 }
-
-
